f9fa5537dade166c0a5b97723154601a6deb4e0e
Laura Ellen de Souza Santos <laura.santos@gclaims.com.br>
2021-10-02 11:12:45 -0300

    add classes

---
+++ src/main/java/Pessoa.java
@@ -0,0 +1,19 @@
+public class Pessoa {
+    public String nome;
+    public String cpf;
+    public Endereco enderecos[];
+
+
+    public Pessoa(String nome, String cpf, int qtdEnd) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public void addEndereco(Endereco end) {
+
+    }
+
+    public void mostraInfo() {
+
+    }
+}
